fd86c6d425addb3a0dee865bbe7a02dd40810c1b
Roxxy Rafael <rafaelroxxy@gmail.com>
2025-06-24 08:42:32 +0800

    refactor: remove unnecessary screen and ui components

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -19,8 +19,9 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <StatusBar style="auto" />
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+export default function AppLayout() {
+
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
